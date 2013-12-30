9e4b083f985e45e660d03d80f66d630800131d58
Rob Reed <github@linuxjunkie.com>
2013-12-30 13:47:10 -0800

    overlay changed: Progress bar is now static with Percent and Watched indicator is a white check mark with a transparent orange background

--- poster.php
+++ poster.php
@@ -6,6 +6,10 @@
    * @author Rob Reed (ljunkie)
    * @copyright 2013 Rob Reed
    * @lincense    Mit Style
+   *
+   *  Created: 2013-12-27
+   * Modified: 2013-12-30
+   *
    */
 
 require_once 'config.php';
@@ -28,185 +32,190 @@ if (is_array($_GET)) {
     }
   }
   if (!empty($thumb_base) and !empty($thumb_vars))  $url = $thumb_base . $thumb_vars;
-
+  if (!isset($progress)) $progress=0;
+  
+  // continue with a valid image
   if (!empty($url)) {
     $base_dir = dirname(__FILE__);
 
-    $src_image = imagecreatefromjpeg($url);
+    // image size ( cropping is possible -- not needed )
     $size = getimagesize($url);
-    $dst_image = imagecreatetruecolor($size[0],$size[1]);
+    $dst_x = 0;   // X-coordinate of destination point. 
+    $dst_y = 0;   // Y --coordinate of destination point. 
+    $src_x = 0;   // Crop Start X position in original image
+    $src_y = 0;   // Crop Srart Y position in original image
+    $img_w = $size[0];
+    $img_h = $size[1];
+
+    // 
+    $im = imagecreatefromjpeg($url);
     
     // colors 
-    $orange_alpha = imagecolorallocatealpha($dst_image, 255, 165, 0, 10);
-    $white_alpha = imagecolorallocatealpha($dst_image, 255, 255, 255, 30);
-    $red = imagecolorallocatealpha($dst_image, 255, 0, 0, 40);
-    $white = imagecolorallocate($dst_image, 255, 255, 255);
-    $grey =  imagecolorallocate($dst_image, 128, 128, 128);
-    $black = imagecolorallocate($dst_image, 0, 0, 0);
-
-
+    $orange_alpha = imagecolorallocatealpha($im, 249, 159, 27, 40);
+    $white_alpha  = imagecolorallocatealpha($im, 255, 255, 255, 40);
+    $red_alpha    = imagecolorallocatealpha($im, 255, 0, 0, 40);
+    $white        = imagecolorallocate($im, 255, 255, 255);
+    $grey         = imagecolorallocate($im, 128, 128, 128);
+    $black        = imagecolorallocate($im, 0, 0, 0);
 
     // font
     $font_dir = $base_dir . '/fonts/';
     $style = "din1451alt.ttf";
     $font = $font_dir.$style;
     $font_size = 18;
-
-    // image size ( cropping is possible )
-    $dst_x = 0;   // X-coordinate of destination point. 
-    $dst_y = 0;   // Y --coordinate of destination point. 
-    $src_x = 0;   // Crop Start X position in original image
-    $src_y = 0;   // Crop Srart Y position in original image
-    $dst_w = $size[0];
-    $dst_h = $size[1];
-    $src_w = $dst_w;
-    $src_h = $dst_h;
     
+    // this could use some work
     
-    // this needs some work
-    $pad_top=.35;
-    $pad_right=.2;
-
-    // landscape(ish)
+    $pad_top=.35;  // we have to pad the top due to the Plex/Channel stretching images to fit
+    $pad_right=.2; // same goes for padding on the right
+    
     // Portrait
-    if ($src_h/$src_w > 1.4) {
+    if ($img_h/$img_w > 1.4) {
       $pad_top=.35;
       $pad_right=.2;
     }
     // 16x9
-    else if ($src_w/$src_h > 1.5) {
+    else if ($img_w/$img_h > 1.5) {
       $pad_top=.35;
       $pad_right=.2;
-    } else {
+    }
+    // landscape(ish)
+    else {
       $pad_top= 1;
       $pad_right=.2;
     }
     
-    // create the dst_image from the poster image
-    imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
-    
-    // calculate the width of bar indicator  
+    /* deprecated - // calculate the width of bar indicator  
     $max = 100;
-    $progress=0;
-    if(isset($_GET['progress'])) $progress = $_GET['progress'];
     $progress_width = round(($progress * ($size[0]-3)) / 100);  
     $full_width = round((100 * ($size[0]-3)) / 100);  
-
-    // some bad math to allow random sizes
-    $expected_width = 200;
+    */
     
     // this are also the bare mininums ( if the font size is smaller )
-    $min_font_size = 20;
+    $min_font_size  = 16;
+    $min_bar_height = 25;
     
     $default_bar_height = 30;
-    $default_dot_size = 18;
+    $default_dot_size   = 18;
+    $default_check_size = 18;
     
+    $bar_height = $default_bar_height; // progress bar ( now background of progress/check mark )
+    $dot_size   = $default_dot_size;
     
-    $bar_height = $default_bar_height;
-    $dot_size = $default_dot_size;
-    
+    // some bad math to allow random sizes
     // if image is smaller than expected
-    if ($size[0]/$expected_width < 1) {
-      $mp = $size[0]/$expected_width;
+    $expected_width = 200;
+    if ($img_w/$expected_width < 1) {
+      $mp = $img_w/$expected_width;
       $dot_size   = ceil($dot_size*$mp);
       $bar_height = ceil($bar_height*$mp);
       $font_size  = ceil($font_size*$mp);
     }
     
     // if image is larger than expected
-    if ($size[0]/$expected_width > 1.2) {
-      $mp = $size[0]/$expected_width;
+    if ($img_w/$expected_width > 1.2) {
+      $mp = $img_w/$expected_width;
       $dot_size   = ceil($dot_size*$mp);
       $bar_height = ceil($bar_height*$mp);
       $font_size  = ceil($font_size*$mp);
     }
     
     // if font size is too small.. go with defaults
-    if ($font_size < $min_font_size) {
+    $isMin = false;
+    if ($font_size < $min_font_size or $bar_height < $min_bar_height) {
+      $isMin = true;
       $font_size = $min_font_size;
       $mp = $min_font_size/$font_size;
       $dot_size   = $default_dot_size;
-      $bar_height = $default_bar_height;
+      $bar_height = $min_bar_height;
     }
     
 
-    $text_height = $bar_height*.85;
+    $text_height = $bar_height*.90;
+    
     // fill the indicator bar
     $x1 = 0;
     $y1 = 0+($bar_height*$pad_top);
     $x2 = $progress_width;
     $y2 = $bar_height+($bar_height*$pad_top);
 
+    $check_box_size = $bar_height+1;
+    
     // vertical center of progress bar for dot
-    $center = $y1+(($y2-$y1)/2);
-    // end
+    $dot_center = $y1+(($y2-$y1)/2);
+    
+    /***  progress area  -- we will still setup the area to keep the formatting consistent even if we don't have progress set **/
+    $pos_right =  $img_w-($img_w*$pad_right);
+    
+    // check padding and width ( overrun )
+    $right_padding = ($check_box_size*$pad_right);
+    $ov_width = $pos_right+$check_box_size+$right_padding; // overlay width
+    if ($ov_width >= $img_w) { $pos_right = $pos_right - ( $ov_width - $img_w); }
     
 
+    // fill in the area/text with the progress
+    $text = round($_GET['progress']) . "%";
+    
+    //testing
+    $pos_left = $pos_right-($check_box_size*1.5);
+    
+    if ($isMin and $progress >= 10 and $progress != 11) { $text_mp = 1.4;  }
+    else if ($progress >= 10)       { $text_mp = 1.25; } 
+    else                            { $text_mp = 1.1;  }
+    
+    $text_left = $pos_right-($check_box_size*$text_mp);
+    
+    if(!isset($_GET['watched']) or $_GET['watched'] == 0) {
+      $pos_left  = $pos_left+$check_box_size;
+      $pos_right = $pos_right+$check_box_size;
+      $text_left = $text_left+$check_box_size;
+    }
+    
+    if ($progress > 0 and $progress < 100) {
+      imagefilledrectangle($im, $pos_left, $y1, $pos_right, $y2, $white_alpha);  
+      imagettftext($im, $font_size, 0, $text_left, $text_height+($text_height*$pad_top), $black, $font, $text);
+    }
+    
 
-    // Watched status
-    imagealphablending($dst_image,true);
+    /**  Watched status ( DOT or Check Mark ) **/
+    imagealphablending($im,true);
     if (INDICATOR == "DOT") {
       // half dot if partially watched
-      if ($progress > 0 ) {
-	imageSmoothArc ($dst_image ,  $size[0]-($size[0]*$pad_right) , $center  , $dot_size , $dot_size ,  array( 255, 165, 0, 5 ),  30, M_PI/2 );
-      }
+      if ($pos_right) {	$pos_left=$pos_right; }
+      else            { $pos_left =  $img_w-($img_w*$pad_right); }
+      $pos_left = $pos_left*(1.1);
+      
+      if ($progress > 0 ) {	imageSmoothArc ($im ,  $pos_left , $dot_center  , $dot_size , $dot_size ,  array( 255, 165, 0, 5 ),  30, M_PI/2 ); }
       // full dot if NOT watched ( plex style )
-      else if(!isset($_GET['watched']) or $_GET['watched'] == 0) {
-	imageSmoothArc ($dst_image ,  $size[0]-($size[0]*$pad_right) , $center  , $dot_size , $dot_size ,  array( 255, 165, 0, 5 ),  M_PI/2, 0 );
-      } 
+      else if(!isset($_GET['watched']) or $_GET['watched'] == 0) { imageSmoothArc ($im ,  $pos_left, $dot_center  , $dot_size , $dot_size ,  array( 255, 165, 0, 5 ),  M_PI/2, 0 ); }
+      
     }
-
+    
     if (INDICATOR == "CHECK") {
       // mark it if watched
       if(isset($_GET['watched']) and $_GET['watched'] == 1) {
 	$overlay = imagecreatefrompng('images/check-mark-512.png');
-	$pos_right =  $size[0]-($size[0]*$pad_right);
+	// use the last right position if set for the new left
+	if ($pos_right) { $pos_left = $pos_right; }
+	else            { $pos_left = $img_w-($img_w*$pad_right); }
+	$pos_right =  $pos_left+$check_box_size;
 	
-	// set the mininum height
-	$h_w = $size[0]*.1358;
-	if ($h_w < 25) $h_w = 25; // min height for checkbox is 20x20
+	$right_padding = ($check_box_size*$pad_right);
+	$ov_width = $pos_right+$right_padding; // overlay width
+	if ($ov_width > $img_w) {
+	  $pos_right = $pos_right - ($ov_width - $img_w);
+	  $pos_left  = $pos_left  - ($ov_width - $img_w);
+	}
 	
-	// verify the image will not overrun the right
-	$diff = $size[0]-$pos_right;
-	if ($diff < $h_w) $pos_right = $size[0] - ($h_w*1.1);
-
-	$start = $size[0]*.94828;
-	$end = $size[0]*.77589;
-
-	
-	if ($start-$end < $h_w) $end = $start-$h_w;
-	//imagefilledrectangle($dst_image, $start, $y1, $end, $y2, $white_alpha);  
-	imagecopyresampled($dst_image, $overlay, $pos_right, $y1*1.12, 0,0, $h_w, $h_w,  512, 512);  
+	imagefilledrectangle($im, $pos_left, $y1, $pos_right, $y2, $orange_alpha);        
+	imagecopyresampled($im, $overlay, $pos_left, $y1, 0,0, $check_box_size, $check_box_size,  512, 512);  
       }
     }
-    // end watched
- 
-
-
-    if ($progress > 0) {
-      
-      if ($progress >= 10) {
-	$min_progress_width = ceil(38*$mp);
-      } else {
-	$min_progress_width = ceil(28*$mp);
-      }
-      if ($progress_width < $min_progress_width)  $x2 = $min_progress_width;
-      
-      imagefilledrectangle($dst_image, $x1, $y1, $x2, $y2, $white_alpha);  
-      
-      // text for indicator bar
-      $text = round($_GET['progress']) . "%";
-      //imagettftext($dst_image, 20, 0, $progress_width/2.001, 25, $grey, $font, $text);
-      $text_left = $progress_width-($font_size*2);
-      if ($text_left < 3) $text_left = 3;
-      imagettftext($dst_image, $font_size, 0, $text_left, $text_height+($text_height*$pad_top), $black, $font, $text);
-      // end
-    }
-
-   
-    // Don't forget to output a correct header
+    
+    
+    /**  Don't forget to output a correct header **/
     header('Content-Type: image/jpg');
-    imagejpeg($dst_image);
+    imagejpeg($im);
   }
 }
 
